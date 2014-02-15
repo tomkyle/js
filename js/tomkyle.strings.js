180771db08eea509203a0daaba547540727d8b9c
Carsten Witt <carsten.witt@gmail.com>
2014-02-15 11:15:56 +0100

    Added quote method

--- js/tomkyle.strings.js
+++ js/tomkyle.strings.js
@@ -32,3 +32,52 @@ if (!String.prototype.trim) {
         return this.replace(/^\s*(\S*(?:\s+\S+)*)\s*$/, "$1");
     };
 }
+
+
+/**
+ * quote()
+ *
+ * Produces a quoted string. This method returns a string
+ * that is like the original string except that it is wrapped
+ * in quotes and all quote and backslash characters are
+ * preceded with backslash.
+ *
+ * @see  http://javascript.crockford.com/remedial.html
+ */
+if (!String.prototype.quote) {
+    String.prototype.quote = function () {
+        var c, i, l = this.length, o = '"';
+        for (i = 0; i < l; i += 1) {
+            c = this.charAt(i);
+            if (c >= ' ') {
+                if (c === '\\' || c === '"') {
+                    o += '\\';
+                }
+                o += c;
+            } else {
+                switch (c) {
+                case '\b':
+                    o += '\\b';
+                    break;
+                case '\f':
+                    o += '\\f';
+                    break;
+                case '\n':
+                    o += '\\n';
+                    break;
+                case '\r':
+                    o += '\\r';
+                    break;
+                case '\t':
+                    o += '\\t';
+                    break;
+                default:
+                    c = c.charCodeAt();
+                    o += '\\u00' + Math.floor(c / 16).toString(16) +
+                        (c % 16).toString(16);
+                }
+            }
+        }
+        return o + '"';
+    };
+}
